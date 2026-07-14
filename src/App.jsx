39aa2ed1ffbb1c0fed8a7ac281564e5d54030091
Ansh Dhama <anshxit26@gmail.com>
2026-07-14 20:00:41 +0530

    Day 9: Enhance React Todo App with advanced features

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,16 @@ function App(){
     if(task.trim() === ""){
       return;
     }
+    const duplicateTask = tasks.some(
+  (item) => item.text.toLowerCase() === task.trim().toLowerCase()
+  );
 
-    setTasks([...tasks, task]);
+    if (duplicateTask) {
+      alert("Task already exists!");
+      return;
+    }
+
+    setTasks([...tasks,{text: task,completed: false},]);
     setTask("");
   }
 
@@ -17,18 +25,49 @@ function App(){
     const updatedTasks = tasks.filter((item, index) => {return index !== indexToDelete});
     setTasks(updatedTasks);
   }
+
+  function toggleTask(indexToToggle){
+    const updatedTasks = tasks.map((item, index) => {
+      if(index === indexToToggle){
+        return {...item, completed: !item.completed};
+      }
+      return item;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function clearAllTasks(){
+    const confirmClear = window.confirm("Are you sure you want to clear all tasks?");
+    if (!confirmClear) {
+      return;
+    }
+    setTasks([]);
+  }
+
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((item) => item.completed).length;
+  const remainingTasks = totalTasks - completedTasks;
+
   return(
     <div className="container">
       <h1>React ToDo App</h1>
 
       <div className="input-section">
-        <input type="text" placeholder="Enter a task..." value={task} onChange={(e) => setTask(e.target.value)} />
+        <input type="text" placeholder="Enter a task..." value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(event) => {if(event.key==="Enter"){addTask();}}} />
         <button onClick={addTask}>Add</button>
       </div>
          <p>You typed: {task}</p>
+         <p>Total Tasks: {totalTasks}</p>
+         <p>Completed Tasks: {completedTasks}</p>
+         <p>Remaining Tasks: {remainingTasks}</p>
       <ul>
-        {tasks.map((item, index) => (<li key={index}>{item} <button onClick={() => deleteTask(index)}>Delete</button></li>))}
+        {tasks.length === 0?(<p>No tasks yet. Add your first task 🚀</p>):(
+        tasks.map((item, index) => (<li key={index} onClick={() => toggleTask(index)} style={{textDecoration: item.completed ? "line-through" : "none",cursor: "pointer"}}>
+          {item.text} <button onClick={(event) => {event.stopPropagation(); deleteTask(index)}}>🗑️</button>
+        </li>)))}
       </ul>
+
+      <button className="clear-btn" onClick={clearAllTasks}>Clear All Tasks</button>
     </div>
   );
 }
