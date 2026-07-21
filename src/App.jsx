0514016a5c83f1c30a373fbb88ed721b58f01708
Ansh Dhama <anshxit26@gmail.com>
2026-07-21 23:35:33 +0530

    Day 10: Add localStorage persistence and task editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App(){
   const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+  const savedTasks = localStorage.getItem("tasks");
+  return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+  console.log("Saving tasks:", tasks);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function addTask(){
     if(task.trim() === ""){
       return;
     }
     const duplicateTask = tasks.some(
-  (item) => item.text.toLowerCase() === task.trim().toLowerCase()
-  );
+     (item, index) =>
+     item.text.toLowerCase() === task.trim().toLowerCase() &&
+     index !== editIndex
+    );
 
     if (duplicateTask) {
       alert("Task already exists!");
       return;
     }
 
-    setTasks([...tasks,{text: task,completed: false},]);
+    if (editIndex !== null) {
+      const updatedTasks = tasks.map((item, index) => {
+        if (index === editIndex) {
+          return { ...item, text: task };
+        }
+        return item;
+      });
+      setTasks(updatedTasks);
+      setEditIndex(null);
+    } else {
+      setTasks([...tasks, { text: task, completed: false }]);
+    }
     setTask("");
   }
 
@@ -44,6 +66,11 @@ function App(){
     setTasks([]);
   }
 
+  function editTask(indexToEdit) {
+  setTask(tasks[indexToEdit].text);
+  setEditIndex(indexToEdit);
+}
+
   const totalTasks = tasks.length;
   const completedTasks = tasks.filter((item) => item.completed).length;
   const remainingTasks = totalTasks - completedTasks;
@@ -54,7 +81,9 @@ function App(){
 
       <div className="input-section">
         <input type="text" placeholder="Enter a task..." value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(event) => {if(event.key==="Enter"){addTask();}}} />
-        <button onClick={addTask}>Add</button>
+        <button onClick={addTask}>
+        {editIndex !== null ? "Update" : "Add"}
+        </button>
       </div>
          <p>You typed: {task}</p>
          <p>Total Tasks: {totalTasks}</p>
@@ -63,7 +92,22 @@ function App(){
       <ul>
         {tasks.length === 0?(<p>No tasks yet. Add your first task 🚀</p>):(
         tasks.map((item, index) => (<li key={index} onClick={() => toggleTask(index)} style={{textDecoration: item.completed ? "line-through" : "none",cursor: "pointer"}}>
-          {item.text} <button onClick={(event) => {event.stopPropagation(); deleteTask(index)}}>🗑️</button>
+          {item.text} 
+          <button onClick={(event) => {
+           event.stopPropagation();
+           editTask(index);
+          }}
+          >
+          ✏️
+         </button>
+
+        <button onClick={(event) => {
+         event.stopPropagation();
+         deleteTask(index);
+        }}
+        >
+         🗑️
+        </button>
         </li>)))}
       </ul>
 
